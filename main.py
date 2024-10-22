984caa6ff8bb080337c2719daa75c99bc54a7da2
Isurumuni WIjesooriya <muniya123135@gmail.com>
2024-10-22 13:29:08 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 
-from API import employee, users
+from API import employee, users,Leavings,supervisor
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this for specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Ensure POST is allowed
+    allow_headers=["*"],
+)
 app.include_router(employee.router)
 app.include_router(users.router)
-# app.include_router(Leavings.router)
-# app.include_router(supervisor.router)
+app.include_router(Leavings.router)
+app.include_router(supervisor.router)
 
 
 @app.get("/")
